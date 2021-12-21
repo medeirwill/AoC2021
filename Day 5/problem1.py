05774efe3441631d975769079ed273b9c5777434
Will Medeiros <william.medeiros@student.sl.on.ca>
2021-12-20 22:06:58 -0500

    Day 5 part one reworked and completed

--- Day 5/problem1.py
+++ Day 5/problem1.py
@@ -1,26 +1,23 @@
-from shapely.ops import unary_union, LineString, MultiLineString
-segmentList = list()
+hitMarkers = dict()
 with open("input.txt", "r") as line_file:
     for line in line_file:
         line = line.strip()
         beginString, endString = line.split(" -> ")
         beginCoords = tuple(int(x) for x in beginString.split(","))
         endCoords = tuple(int(x) for x in endString.split(","))
-        if beginCoords[0] == endCoords[0] or beginCoords[1] == endCoords[1]:
-            segmentList.append(LineString([beginCoords, endCoords]))
-
-overlapObject = segmentList[0].intersection(segmentList[1])
-unionObject = unary_union(segmentList[0:1])
-overlapCoordsSet = set(overlapObject.coords)
-
-for i in range(2, len(segmentList)):
-    overlapObject = unionObject.intersection(segmentList[i])
-    try:
-        overlapCoordsSet.add(overlapObject.coords)
-    except NotImplementedError:
-        for geom in overlapObject.geoms:
-            overlapCoordsSet.add(geom.coords)
+        if beginCoords[0] == endCoords[0]:
+            directionalModifier = 1 if endCoords[1] > beginCoords[1] else -1
+            for y in range(beginCoords[1], endCoords[1] + directionalModifier, directionalModifier):
+                try:
+                    hitMarkers[(beginCoords[0], y)] += 1
+                except KeyError:
+                    hitMarkers[(beginCoords[0], y)] = 1
+        elif beginCoords[1] == endCoords[1]:
+            directionalModifier = 1 if endCoords[0] > beginCoords[0] else -1
+            for x in range(beginCoords[0], endCoords[0] + directionalModifier, directionalModifier):
+                try:
+                    hitMarkers[(x, beginCoords[1])] += 1
+                except KeyError:
+                    hitMarkers[(x, beginCoords[1])] = 1
             
-    unionObject = unary_union([unionObject, segmentList[i]])
-
-print(len(overlapCoordsSet))        
+print(len([coords for (coords, hits) in hitMarkers.items() if hits > 1]))
