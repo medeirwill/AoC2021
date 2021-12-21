149b7c6299e1460ad5b925d010767539eaf17692
Will Medeiros <william.medeiros@student.sl.on.ca>
2021-12-21 01:53:37 -0500

    Day 9 part 1

---
+++ Day 9/problem1.py
@@ -0,0 +1,30 @@
+class CaveFloorPoint:
+    def __init__(self,height, leftNeighbour, upNeighbour):
+        self.height = height
+        self.risky = True
+        self.lowFrom = dict()
+        self.risky = self.risky and (leftNeighbour is None or leftNeighbour.height > self.height)
+        self.risky = self.risky and (upNeighbour is None or upNeighbour.height > self.height)
+        if leftNeighbour is not None:
+            leftNeighbour.risky = leftNeighbour.risky and (leftNeighbour.height < self.height)
+        if upNeighbour is not None and upNeighbour.risky: 
+            upNeighbour.risky = upNeighbour.risky and (upNeighbour.height < self.height)
+
+with open("input.txt", "r") as caveFloorFile:
+    caveRows = [line.strip() for line in caveFloorFile.readlines()]
+
+floorPointsByCoords = dict()
+for rowNum, row in enumerate(caveRows):
+    for columnNum, height in enumerate([int(heightString) for heightString in row]):
+        try:
+            leftNeighbour = floorPointsByCoords[(columnNum - 1, rowNum)]
+        except KeyError:
+            leftNeighbour = None
+        try:
+            upNeighbour = floorPointsByCoords[(columnNum, rowNum - 1)]
+        except KeyError:
+            upNeighbour = None
+        
+        floorPointsByCoords[(columnNum, rowNum)] = CaveFloorPoint(height, leftNeighbour, upNeighbour)
+            
+print(sum([1 + point.height for point in floorPointsByCoords.values() if point.risky]))
